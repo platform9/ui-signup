27a6b5d2fc62d9f7dc1ff74efb5e10fcf3c2fabf
John Ruddell <johnruddell@gmail.com>
2021-08-23 23:50:10 -0600

    WIP, setting up embark apis

--- src/actions.ts
+++ src/actions.ts
@@ -1,9 +1,28 @@
 import { ViewPanes } from './constants'
+import { IUser } from './context'
 import browserHistory from './history'
 
-export const navigate = (
-  pane: ViewPanes,
-  path = window.location.pathname
-) => {
+const baseUrl = process.env.BUILD_TARGET === 'production' ? '/' : 'http://localhost:3000/'
+
+export const navigate = (pane: ViewPanes, path = window.location.pathname) => {
   browserHistory.push({ route: `${path}?view=${pane}` })
+}
+
+export const createEmbarkUser = async (user: IUser) => {
+  fetch(`${baseUrl}/v1/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.error) {
+        throw new Error(res.error)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 }
