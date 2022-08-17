61e51179a2a79980764c54c48c807c4101123f67
chrisj-platform9 <51425288+chrisj-platform9@users.noreply.github.com>
2022-08-17 11:56:32 -0700

    Update index.tsx

--- src/pages/page-backdrop/index.tsx
+++ src/pages/page-backdrop/index.tsx
@@ -12,7 +12,6 @@ export default function PageBackdrop() {
   return (
     <div className="uiSignupAppPageBackdrop">
       <div className="uiSignupAppPageBackdropGradient" />
-      <DownloadAppCtl />
       <ManagementPlane />
     </div>
   )
